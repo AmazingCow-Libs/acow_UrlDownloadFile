2bd05ef6f4f4008ecd64a0640ac3fba68181de20
n2omatt <n2omatt@amazingcow.com>
2018-04-17 11:20:16 -0700

    - Change namespace and cosmetic changes.

--- acow/include/URLDownloadFile.h
+++ acow/include/URLDownloadFile.h
@@ -4,39 +4,37 @@
 #include <string>
 // AmazingCow Libs
 #include "acow/cpp_goodies.h"
-// acow_URLDownloadFile
-#include "URLDownloadFile_Utils.h"
 
-namespace acow { namespace URLDownloadFile { 
+namespace acow { namespace net { namespace URLDownloadFile {
 
 //----------------------------------------------------------------------------//
 // Types                                                                      //
 //----------------------------------------------------------------------------//
 typedef unsigned long errorcode_t;
 
+
 //----------------------------------------------------------------------------//
 // Functions                                                                  //
 //----------------------------------------------------------------------------//
 ///-----------------------------------------------------------------------------
 bool DownloadData(
-	const std::string &url,
-	u8	     		 **pOutValue, 
-	size_t			  *pValueSize, 
-	errorcode_t		  *pOutError = nullptr) noexcept;
-
+    const std::string &url,
+    u8              **pOutValue,
+    size_t           *pValueSize,
+    errorcode_t      *pOutError = nullptr) noexcept;
 
 ///-----------------------------------------------------------------------------
 bool DownloadString(
-	const std::string &url, 
-	std::string		  *pOutString, 
-	errorcode_t		  *pOutError = nullptr) noexcept;
+    const std::string &url,
+    std::string          *pOutString,
+    errorcode_t          *pOutError = nullptr) noexcept;
 
 ///-----------------------------------------------------------------------------
 bool DownloadToFile(
-	const std::string &url, 
-	const std::string &fullpath,
-	errorcode_t		  *pOutError = nullptr) noexcept;
-
+    const std::string &url,
+    const std::string &fullpath,
+    errorcode_t          *pOutError = nullptr) noexcept;
 
 } // namepace URLDownloadFile
+} // namespace net
 } // namespace acow
