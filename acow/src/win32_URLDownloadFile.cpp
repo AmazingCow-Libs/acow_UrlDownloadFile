44513bc48bdb7ea50643c18ce796ae146b91ed4b
n2omatt <n2omatt@amazingcow.com>
2018-04-17 11:21:16 -0700

    - Continue the implemenation for windows.

--- acow/src/win32_URLDownloadFile.cpp
+++ acow/src/win32_URLDownloadFile.cpp
@@ -1,5 +1,5 @@
 // Header
-#include "include/URLDownloadFile.h"
+#include "acow/include/URLDownloadFile.h"
 
 #if (ACOW_OS_IS_WINDOWS)
 
@@ -11,125 +11,102 @@
 #define _WINSOCKAPI_
 #include <Windows.h>
 #include <Urlmon.h>
+// AmazingCow Libs
+#include "acow/IO.h"
 
+//----------------------------------------------------------------------------//
+// Library Linking                                                            //
+//----------------------------------------------------------------------------//
 #pragma comment(lib, "Urlmon.lib")
 
 // Usings
 using namespace acow;
+using namespace net;
 using namespace std;
 
-//----------------------------------------------------------------------------//
-// Macros                                                                     //
-//----------------------------------------------------------------------------//
-#define SAFE_ASSIGN(_var_, _value_)  \
-	do {							 \
-		if (_var_) *_var_ = _value_; \
-	} while (0) 
-
-
-//----------------------------------------------------------------------------//
-// Helper Functions                                                           //
-//----------------------------------------------------------------------------//
-acow_internal_function std::string 
-GetTempFilename() noexcept
-{
-	// COWTODO(n2omatt): Implement...
-	return "_temp_file_";
-}
-
-acow_internal_function size_t 
+acow_internal_function size_t
 FindFileSize(FILE *pFile) noexcept
 {
-	// COWTODO(n2omatt): Error handling...
-	auto curr = ftell(pFile);
-	fseek(pFile, 0, SEEK_END);
-	auto len = ftell(pFile);
-	fseek(pFile, curr, SEEK_SET);
+    // COWTODO(n2omatt): Error handling...
+    auto curr = ftell(pFile);
+    fseek(pFile, 0, SEEK_END);
+    auto len = ftell(pFile);
+    fseek(pFile, curr, SEEK_SET);
 
-	return len;
+    return len;
 }
 
 //----------------------------------------------------------------------------//
 // Implementation                                                             //
 //----------------------------------------------------------------------------//
 //------------------------------------------------------------------------------
-bool 
+bool
 URLDownloadFile::DownloadData(
-	const std::string &url,
-	u8   			 **pOutValue, 
-	size_t			  *pValueSize,
-	errorcode_t		  *pOutError) noexcept
+    const std::string &url,
+    u8               **pOutValue,
+    size_t            *pValueSize,
+    errorcode_t       *pOutError) noexcept
 {
-	auto temp_filename = GetTempFilename();
-	if(DownloadToFile(url, temp_filename, pOutError)) {
-		// Read entire file.
-		auto p_file	= fopen(temp_filename.c_str(), "rb"); 
-		// COWTODO(n2omatt): Check if we could open the file...
+    auto temp_filename = acow::IO::Path::GetTempFileName();
+    if(DownloadToFile(url, temp_filename, pOutError)) {
+        auto file_bytes      = acow::IO::File::ReadAllBytes(temp_filename);        
+        auto file_bytes_size = file_bytes.size();
+        auto buffer          = acow_malloc(ACOW_SIZEOF(u8, file_bytes_size));
+        
+        acow_memcpy(buffer, file_bytes.data(), file_bytes_size);
+        *pOutValue = (u8*)buffer;
 
-        //COWTODO(n2omatt): acow_c_goodies has some stuff like that?
-		auto file_len = FindFileSize(p_file);
+        ACOW_SAFE_ASSIGN(pValueSize, file_bytes_size);
+        ACOW_SAFE_ASSIGN(pOutError,  0);
 
-		// COWTOTOD(n2omatt); Check if we could alloc memory...
-		auto buffer = (u8 *)malloc(file_len * sizeof(u8));;
-		*pOutValue = buffer;
+        return true;
+    }
 
-		// COWTODO(n2omatt): Check if read_size == file_len;
-		auto read_size = fread(*pOutValue, file_len, 1, p_file);
-		
-		fclose(p_file);
-		
-		SAFE_ASSIGN(pValueSize, file_len);
-		SAFE_ASSIGN(pOutError,  0);
-		
-		return true;
-	}
-
-	return false;
+    return false;
 }
 
 //------------------------------------------------------------------------------
-bool 
+bool
 URLDownloadFile::DownloadString(
-	const std::string &url,
-	std::string		  *pOutString,
-	errorcode_t		  *pOutError) noexcept
+    const std::string &url,
+    std::string          *pOutString,
+    errorcode_t          *pOutError) noexcept
 {
-	u8     *p_contents    = nullptr;
-	size_t  contents_size = 0;
-	
-	if(DownloadData(url, &p_contents, &contents_size, pOutError)) {
-		
-		pOutString->resize(contents_size + 1);
-		pOutString->assign(reinterpret_cast<char *>(p_contents), contents_size);
+    u8     *p_contents    = nullptr;
+    size_t  contents_size = 0;
 
-		free(p_contents);
-	}
-	
-	return false;
+    if(DownloadData(url, &p_contents, &contents_size, pOutError)) {
+        pOutString->resize(contents_size + 1);
+        pOutString->assign(reinterpret_cast<char *>(p_contents), contents_size);
+
+        acow_free(p_contents);
+    }
+
+    return false;
 }
 
 //------------------------------------------------------------------------------
-bool  
+bool
 URLDownloadFile::DownloadToFile(
-	const std::string &url,
-	const std::string &fullpath, 
-	errorcode_t		  *pOutError) noexcept
-{	
-	auto hresult = URLDownloadToFile(
-		NULL, 
-		url		.c_str(), 
-		fullpath.c_str(), 
-		0, 
-		NULL
-	);
+    const std::string &url,
+    const std::string &fullpath,
+    errorcode_t       *pOutError) noexcept
+{
+    auto hresult = URLDownloadToFile(
+        NULL,
+        url     .c_str(),
+        fullpath.c_str(),
+        0,
+        NULL
+    );
 
-	if(SUCCEEDED(hresult)) {
-		SAFE_ASSIGN(pOutError, 0);
-		return true;
-	} else {
-		SAFE_ASSIGN(pOutError, hresult);
-		return false;
-	}
+    if(SUCCEEDED(hresult)) {
+        ACOW_SAFE_ASSIGN(pOutError, 0);
+        return true;
+    } else {
+        ACOW_SAFE_ASSIGN(pOutError, hresult);
+        return false;
+    }
 }
 
 #endif // ACOW_OS_IS_WINDOWS
